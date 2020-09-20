f7abd8506efa35b7609223781c84cb909affb45e
Jerzy Balwinski <jurek_b@wp.pl>
2020-09-20 16:10:41 +0200

    Sample HelloController.class added - Rest Controller

--- src/main/java/javapoz24/team3/rental/application/api/HelloControler.java
+++ src/main/java/javapoz24/team3/rental/application/api/HelloControler.java
@@ -1,2 +1,13 @@
-package javapoz24.team3.rental.application.api;public class HelloControler {
+package javapoz24.team3.rental.application.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+
+    @GetMapping("/hello")
+    private String helloSpring() {
+        return "Hello - rental app!";
+    }
 }
