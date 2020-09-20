c2ab2e860d0e0f4891cc67c63652361eb2b35067
Jerzy Balwinski <jurek_b@wp.pl>
2020-09-20 14:47:24 +0200

    initial, empty

---
+++ src/main/java/javapoz24/team3/rental/RentalApplication.java
@@ -0,0 +1,13 @@
+package javapoz24.team3.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javapoz24/team3/rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package javapoz24.team3.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
