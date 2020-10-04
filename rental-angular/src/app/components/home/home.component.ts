d105961535b0c6f7fb343d52011836841d8c4538
Korejap <Korejap@users.noreply.github.com>
2020-10-04 09:47:52 +0200

    problem z pojedyńczym serwisem, nie wyświetla się addres i reszta

--- rental-angular/src/app/components/home/home.component.ts
+++ rental-angular/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {CompanyData} from "../../service/company-data";
+import {Address, CompanyData} from "../../service/company-data";
 import {RestService} from "../../service/rest.service";
 
 
@@ -13,13 +13,16 @@ import {RestService} from "../../service/rest.service";
 export class HomeComponent implements OnInit {
 
 companyData : CompanyData;
-
+companyAddress: Address;
 
   constructor(private restService : RestService) { }
 
   ngOnInit(): void {
     this.restService.getCompanyData().subscribe(value => {
       this.companyData = value;
+    });
+    this.restService.getCompanyAddress().subscribe(value1 => {
+      this.companyAddress = value1;
     });
   }
 }
