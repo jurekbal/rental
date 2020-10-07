2f226e57df919ce70cca6efc978973e6740d4125
Jerzy Balwinski <jurek_b@wp.pl>
2020-10-07 20:35:35 +0200

    Employee jpa interfaces

--- src/main/java/javapoz24/team3/rental/domain/emploee/EmployeeDomainService.java
+++ src/main/java/javapoz24/team3/rental/domain/emploee/EmployeeDomainService.java
@@ -1,2 +1,13 @@
-package javapoz24.team3.rental.domain.emploee;public interface EmployeeDomainService {
+package javapoz24.team3.rental.domain.emploee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeDomainService {
+
+    List<Employee> getAllEmployees();
+
+    Optional<Employee> findEmployeeById(Long id);
+
+    List<Employee> getEmployeesByBranchId(Long branchId);
 }
