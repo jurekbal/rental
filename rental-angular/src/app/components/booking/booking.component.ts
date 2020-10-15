d5057ce8254c0c8b34b99d40ac5330bb0ea358d8
Korejap <Korejap@users.noreply.github.com>
2020-10-15 17:11:40 +0200

    getBooking + getBookingId zrobione, addBooking do dokończenia.

--- rental-angular/src/app/components/booking/booking.component.ts
+++ rental-angular/src/app/components/booking/booking.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {RestService} from "../../service/rest.service";
+import {Branch} from "../../service/branch";
+
+class Booking {
+}
+
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css']
+  styleUrls: ['./booking.component.css'],
+
 })
 export class BookingComponent implements OnInit {
+booking: Booking;
 
-  constructor() { }
+
+
+  constructor(private restService : RestService  ) { }
 
   ngOnInit(): void {
+    this.getBooking();
   }
 
+  getBooking(): void{
+    this.restService.getBooking()
+      .subscribe(booking => this.booking = booking);
+  }
+  public addBooking(customerID: number, employeeId: number, carId: number,rentalDay: ,returnDay: ,rentBranchId: number,returnBranchId: number): void{
+  // if (!customerID || !employeeId|| !postalCode ) {
+  // return;
+}
+this.booking = {}
+}
+this.restService.addBooking({customerID, employeeId, carId,rentalDay, returnDay,rentBranchId,returnBranchId } as Booking)
+  .subscribe(booking => {
+    this.booking.push(booking);
+  });
 }
